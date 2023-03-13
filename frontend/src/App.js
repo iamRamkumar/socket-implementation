e88010cb65257a42128e16164a58884a9e2f628a
Ramkumar R3 <ramkumar.r3@ibm.com>
2023-03-13 14:57:55 +0530

    added socket client to react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import {io} from 'socket.io-client'
 
 function App() {
+  const [time, setTime] = useState('fetching');
+  useEffect(() => {
+    const socket = io('http://localhost:5000');
+    socket.on('connect', () => console.log(socket.id));
+    socket.on('connect_error', () => {
+      setTimeout(() => socket.connect, 5000);
+    });
+    socket.on('time', (data) => setTime(data));
+    socket.on('disconnet', () => setTime('sever disconnect'));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {time}
     </div>
   );
 }
